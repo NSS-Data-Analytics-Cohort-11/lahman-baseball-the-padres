73f7592f2d9a1b33736f468309abfe7b784bbede
willisc22 <scwilliams15@gmail.comgitconfig --globsl user.name willisc22>
2024-02-29 21:21:47 -0600

    Through Question 6

--- scripts/lahman_baseball_scripts.sql
+++ scripts/lahman_baseball_scripts.sql
@@ -38,17 +38,91 @@ LIMIT 1;
 SELECT namefirst
 	, namelast
 	, schoolname
-	, playerid
+	, SUM(distinct(CAST(salary AS int)::money))
 FROM people
 INNER JOIN collegeplaying
 	USING (playerid)
 INNER JOIN schools
 	USING (schoolid)
+INNER JOIN salaries
+	USING (playerid)
 GROUP BY namefirst
 	, namelast
 	, schoolname
-	, playerid
 HAVING schoolname LIKE '%Vanderbilt%'
+ORDER BY SUM(distinct(CAST(salary AS int)::money)) DESC
 
 
+WITH vandy_players AS (
+	SELECT DISTINCT playerid
+	FROM collegeplaying
+	WHERE schoolid = 'vandy'
+)
+SELECT 
+	namefirst || namelast AS fullname, 
+	SUM(salary)::int::MONEY AS total_salary
+FROM salaries
+INNER JOIN vandy_players
+USING(playerid)
+INNER JOIN people
+USING(playerid)
+GROUP BY namefirst || namelast
+ORDER BY total_salary DESC;
+
+--ANSWER Q3: Run above query--one with DISTINCT SUM, one with CTE---
+
+
+--QUESTION 4: Using the fielding table, group players into three groups based on their position: 
+--label players with position OF as "Outfield",
+--those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". 
+--Determine the number of putouts made by each of these three groups in 2016.
+
+SELECT SUM(po)
+	, yearid
+	, CASE WHEN pos ilike 'OF' THEN 'Outfield'
+	WHEN pos IN('SS','1B','2B','3B') THEN 'Infield'
+	WHEN pos IN('P','C') THEN 'Battery' END AS player_position
+FROM fielding
+WHERE yearid = '2016'
+GROUP BY player_position, yearid
+
+--ANSWER Q4: Run query above--
+
+
+--Question 5: Find the average number of strikeouts per game by decade since 1920. 
+--Round the numbers you report to 2 decimal places. 
+-- Do the same for home runs per game. Do you see any trends?
+
+SELECT ROUND(SUM(so)*1.0/SUM(g),2) AS avg_strikeouts
+	, ROUND(SUM(hr)*1.0/SUM(g),2) AS avg_homeruns
+	,(yearid)/10*10 AS Decade
+FROM teams
+WHERE yearid >= 1920
+GROUP BY Decade
+ORDER BY Decade
+
+--ANSWER Q5: steroid usage became popular in 1980s, and we see a jump in HR avg per game from 1980s to 1990s and so on
+--The average strikout per game is also generally higher than the average HR per game, therefore you are more likely to strikeout than hit a HR
+
+
+--QUESTION 6: Find the player who had the most success stealing bases in 2016, 
+--where success is measured as the percentage of stolen base attempts which are successful.
+--(A stolen base attempt results either in a stolen base or being caught stealing.) 
+--Consider only players who attempted at least 20 stolen bases.
+
+-- sb/(NULLIFsb,0)+NULLIF(cs,0))*100
+	
+
+SELECT namefirst
+	, namelast
+	, ROUND(sb/NULLIF((sb+cs)*1.0,0)*100,2) AS successful_steals
+FROM people
+INNER JOIN batting
+	USING (playerid)
+WHERE sb/NULLIF((sb+cs),0)*11 IS NOT NULL
+	AND (sb+cs) >= 20
+	AND yearid = 2016
+ORDER BY successful_steals DESC
+
+--ANSWER QUESTION 6: run code above--
 
